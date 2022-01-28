858ef715ee7b24b2d12342c8c0bb116d071137a5
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-01-28 11:52:36 -0800

    fix: error in models.py
    blog app fixed

--- blogs/models.py
+++ blogs/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from users.models import CustomUser
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    subtitle = models,CharField(max_length=200)
-    author = models.ForeignKey('auth.User', on_delete=CASCADE,)
+    subtitle = models.CharField(max_length=200)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE,)
     body = models.TextField()
 
     def __str__(self):
