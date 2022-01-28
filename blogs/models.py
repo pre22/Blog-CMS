f68142d727a61aab1fc43d9960b6e556562b5ef6
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-01-28 11:22:18 -0800

    chore: created blog model

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    subtitle = models,CharField(max_length=200)
+    author = models.ForeignKey('auth.User', on_delete=CASCADE,)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
