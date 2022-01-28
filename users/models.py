486f4bba026fe9edbb2253a2aa9582a149d49437
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-01-28 10:18:17 -0800

    chore: Created CustomUser model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+interests = (
+    ('Hood', 'Hood'),
+    ('Teen', 'Teen'),
+    ('Den', 'Den'),
+    ('Thigh', 'Thigh'),
+)
+class CustomUser(AbstractUser):
+    interest = models.CharField(choice=interests)
+    is_teacher = models.BooleanField('Teacher', default=False)
+    is_student = models.BooleanField('Student', default=False)
