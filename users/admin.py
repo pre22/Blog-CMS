63905089e5f8a0a2452371b51cc8bff9afeede0c
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-01-28 10:43:33 -0800

    chore: registered model in admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.forms import CustomUserCreationForm, CustomUserChangeForm 
+from users.models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm 
+    form = CustomUserChangeForm 
+    model = CustomUser
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
